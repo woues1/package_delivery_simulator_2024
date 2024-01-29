e5241ab886ebb5583524175d1b00c0f331ace5f5
woues1 <toni.hirvikallio@gmail.com>
2024-01-29 19:19:57 +0200

    tweak

--- data/sql_db_connect.py
+++ data/sql_db_connect.py
@@ -1,12 +1,14 @@
 import mysql.connector
 
-def get_db_connection():
+
+def get_db_connection() -> mysql.connector.connect:
+
     db_config = {
         'host': 'localhost',
         'user': 'root',
         'password': 'root',
         'database': 'flight_game',
     }
+
     # Establish a connection
     return mysql.connector.connect(**db_config)
-

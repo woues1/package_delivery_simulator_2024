1343a349297f6c1ae9c9aa4bb0852717514e77e0
woues1 <toni.hirvikallio@gmail.com>
2024-01-29 18:29:52 +0200

    sql_connect päivitys + import esimerkki

--- data/sql_db_connect.py
+++ data/sql_db_connect.py
@@ -1,24 +1,12 @@
 import mysql.connector
 
-# Replace the placeholders with your database credentials
-db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'root',
-    'database': 'flight_game',
-}
+def get_db_connection():
+    db_config = {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'root',
+        'database': 'flight_game',
+    }
+    # Establish a connection
+    return mysql.connector.connect(**db_config)
 
-# Establish a connection
-conn = mysql.connector.connect(**db_config)
-
-# Create a cursor to interact with the database
-cursor = conn.cursor()
-
-# Example: execute a simple query
-cursor.execute("SELECT * FROM airport")
-result = cursor.fetchall()
-print(result)
-
-# Close the cursor and connection when done
-cursor.close()
-conn.close()
---
+++ utils/pelaaja.py
@@ -0,0 +1,11 @@
+from data.sql_db_connect import get_db_connection
+
+connection = get_db_connection()
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM airport")
+result = cursor.fetchall()
+print(result)
+
+cursor.close()
+connection.close()

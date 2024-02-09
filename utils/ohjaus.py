975cb640363cc79cbe5a8827d075aed0e379fdea
Vernere <verner.etola@metropolia.fi>
2024-02-09 21:15:34 +0200

    Joo

--- utils/ohjaus.py
+++ utils/ohjaus.py
@@ -1,12 +1,14 @@
 import keyboard
+
+
 def tapahtuma_paina(on_key_press):
     def on_key_tapahtumaa(tapahtuma):
         nappi = tapahtuma.name
-        if nappi =='esc':
-            on_key_press('esc')#<----- Insert
+        if nappi == 'esc':
+            on_key_press('esc')  # <----- Insert
         elif nappi in [str(i) for i in range(1, 10)]:
-            on_key_press(nappi)#<----- Insert
-        else:
-            print("Invalid key")#<----- Insert
-    
+            on_key_press(nappi)  # <----- Insert
+
+
+
     keyboard.on_press(on_key_tapahtumaa)

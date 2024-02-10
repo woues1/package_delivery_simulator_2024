e985a896378a565d29c485046d650f2fb7cf333a
Vernere <verner.etola@metropolia.fi>
2024-02-11 01:44:46 +0200

    Revert "Pelaaja.py lajennettu pelaaja class + käyttö esimerkit"
    This reverts commit d99cdcf4851e01167e128b2bcab686d3b6e62bd6.

--- utils/pelaaja.py
+++ utils/pelaaja.py
@@ -8,11 +8,6 @@ class Pelaaja:
         self.location = location
 
 
-# Päivittää käyttäjän pistemäärän.
-    def suoritus_piste_lisays(self, pisteet, piste_kerroin):
-        self.pisteet = self.pisteet + pisteet * piste_kerroin
-
-
 def kayttaja_haku():
     result = sql_search(f"SELECT screen_name, co2_budget, location "
                         f"FROM game "
@@ -26,13 +21,5 @@ def olio_luonti():
         return Pelaaja(i[0], i[1], i[2])
 
 
-pelaaja_instanssi = olio_luonti()
-
-
-# pelaaja_instanssin tietojen haku
-print(f"\n{pelaaja_instanssi.nimi} {pelaaja_instanssi.location} {pelaaja_instanssi.pisteet}")
-
-# Käyttää olioa pisteiden lisäämiseen pelaaja_instanssille
-pelaaja_instanssi.suoritus_piste_lisays(1000, 1.2)
-
-print(f"\npäivitettyjen pisteiden määrä: {pelaaja_instanssi.pisteet}")
+pelaaja = olio_luonti()
+print(pelaaja.nimi, pelaaja.location, pelaaja.pisteet)

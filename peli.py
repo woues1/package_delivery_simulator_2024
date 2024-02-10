c9b7f0ff9616a8593e0e11aff76a9eec3b7fdd5f
woues1 <toni.hirvikallio@gmail.com>
2024-02-10 09:37:57 +0200

    peli.py kommentteja, valikko tiedosto lisätty

--- peli.py
+++ peli.py
@@ -1,6 +1,29 @@
+import keyboard
+import utils.pelilauta
+import utils.pelaaja
+
+
 def main():
-    print("Hello")
+    while True:
+        valinta = keyboard.KeyboardEvent
+        if valinta == 1:
+            print("valitsit tehtävän 1: (tehtävä 1 tiedot)")
+        elif valinta == 2:
+            print("valitsit tehtävän 2: (tehtävä 2 tiedot)")
+        elif valinta == 3:
+            print("valitsit tehtävän 2: (tehtävä 2 tiedot)")
+        #päivitetään "pelaaja olioon"
 
 
+    #Tässä katsotaan onko käyttäjä mennyt ESC > valikko > quit.
+    #jos on valinnut quit suljetaan peli
+
+    #ESC keypress löytyy ohjaus kansiosta ja valikko löytyy kansiosta "valikko"
+
+        """
+        if valinta == quit(): 
+            print(f"kiitos pelaamisesta {pelaaja.nimi}")
+            break 
+        """
 if __name__ == '__main__':
     main()
---
+++ utils/valikko.py
@@ -0,0 +1,11 @@
+
+"""
+def valikko():
+    print("MAIN MENU PLACEHOLDER")
+    print("PELAAJAN TIEDOT(pisteet, co2 budjetti jne.)")
+    
+    print("UUSI PELI (poistaa nykyisen pelitiedoston ja aloittaa uudestaan)")
+    
+    print("LOPETA PELI(nappi)")
+        
+"""

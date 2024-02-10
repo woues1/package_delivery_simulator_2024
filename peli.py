3a5f5cd09f60cf663bf9de4040b32c07849815d5
tonih <toni.hirvikallio@gmail.com>
2024-02-10 19:10:40 +0200

    peli.py esimerkki luo_tehtävä käytöstä + komennteja

--- peli.py
+++ peli.py
@@ -1,30 +1,23 @@
 import keyboard
+# import utils.pelaaja
+# import utils.pelilauta
 from utils.ohjaus import kbdCallback
 
-#alemmassa kommentissa esimerkki
-#keyboard.on_press(kbdCallback(keyboard.wait()))
+# alemmassa kommentissa esimerkki
+# keyboard.on_press(kbdCallback(keyboard.wait()))
 
 def main():
     while True:
-        valinta = keyboard.on_press(kbdCallback(keyboard.wait()))
-        if valinta == 1:
-            print("valitsit tehtävän 1: (tehtävä 1 tiedot)")
-        elif valinta == 2:
-            print("valitsit tehtävän 2: (tehtävä 2 tiedot)")
-        elif valinta == 3:
-            print("valitsit tehtävän 2: (tehtävä 2 tiedot)")
-        #päivitetään "pelaaja olioon"
+        for i in range(3):
+            print(f"Tehtävä {i}: {luo_tehtävä()}\n")
+        keyboard.on_press(kbdCallback(keyboard.wait()))
 
+    # tähän kohtaan voi laittaa if target location == player location
+    # Suoritetaan tehtävän palautus
 
-    #Tässä katsotaan onko käyttäjä mennyt ESC > valikko > quit.
-    #jos on valinnut quit suljetaan peli
+    # päivitetään "pelaaja olioon"
 
-    #ESC keypress löytyy ohjaus kansiosta ja valikko löytyy kansiosta "valikko"
+    # ESC keypress löytyy ohjaus kansiosta ja valikko löytyy kansiosta "valikko"
 
-        """
-        if valinta == quit(): 
-            print(f"kiitos pelaamisesta {pelaaja.nimi}")
-            break 
-        """
 if __name__ == '__main__':
     main()

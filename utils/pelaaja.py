c34917f128b8ade8ec3b5be1a2509e10095a3aad
woues1 <toni.hirvikallio@gmail.com>
2024-02-10 08:37:26 +0200

    pelaajan tietojen haku

--- utils/pelaaja.py
+++ utils/pelaaja.py
@@ -3,11 +3,19 @@ from data.sql_db_connect import get_db_connection
 connection = get_db_connection()
 cursor = connection.cursor()
 
-cursor.execute("SELECT * FROM airport")
+cursor.execute("SELECT screen_name, co2_budget, location FROM game where screen_name = 'ilkka'")
 result = cursor.fetchall()
-print(result)
-
 cursor.close()
 connection.close()
 
+class pelaaja:
+    def __init__(self, nimi, pisteet, location):
+        self.nimi = nimi
+        self.pisteet = pisteet
+        self.location = location
 
+
+for i in result:
+    pelaaja = pelaaja(i[0],i[1],i[2])
+
+print(pelaaja.nimi, pelaaja.pisteet, pelaaja.location)

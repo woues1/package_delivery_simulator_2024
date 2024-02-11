864241b02276a2609a37627fca5d652d42a7cf8d
tonih <toni.hirvikallio@gmail.com>
2024-02-11 14:04:28 +0200

    pelaaja.py lisätty tehtävä tila + piste seuranta

--- utils/pelaaja.py
+++ utils/pelaaja.py
@@ -1,25 +1,31 @@
 from data.sql_db_connect import sql_search
 
 
+# pelaajan tiedot
 class Pelaaja:
     def __init__(self, nimi, pisteet, location):
         self.nimi = nimi
         self.pisteet = pisteet
         self.location = location
+        self.tehtava_aktiivinen = False
+
+    # Päivittää pelaajan tehtävä tilan
+    def paivita_tehtava_aktiivinen(self, is_active):
+        self.tehtava_aktiivinen = is_active
+
+    def paivita_pisteet(self, pisteet, p_maara, kerroin):
+        self.pisteet = pisteet + p_maara * kerroin
 
 
 def kayttaja_haku():
-    result = sql_search(f"SELECT screen_name, co2_budget, location "
+    tulos = sql_search(f"SELECT screen_name, co2_budget, location "
                         f"FROM game "
                         f"where screen_name = 'ilkka'")
-    return result
+    return tulos
 
 
+# luo olion, johon tallennetaan pelaajan tiedot
 def olio_luonti():
     res = kayttaja_haku()
     for i in res:
         return Pelaaja(i[0], i[1], i[2])
-
-
-pelaaja = olio_luonti()
-print(pelaaja.nimi, pelaaja.location, pelaaja.pisteet)

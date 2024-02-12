9be90b358744d672d5ff42b5e47810561072e954
Vernere <verner.etola@metropolia.fi>
2024-02-12 10:51:59 +0200

    sql_db_update lisätty loction lookup tehtavan_luonti_algoritmi.py lisätty lisätty rng location

--- data/sql_db_update.py
+++ data/sql_db_update.py
@@ -3,3 +3,8 @@
 #def sql_db_update_new_player():
 
 #def sql_db_updaate_new_game():
+from data.sql_db_connect import sql_search
+def sql_db_lookup_locations():
+    locations_serch= f"SELECT a.ident FROM airport a;"
+    tulos_locations= sql_search(locations_serch)
+    return tulos_locations
--- data/tehtavan_luonti_algoritmi.py
+++ data/tehtavan_luonti_algoritmi.py
@@ -1,9 +1,22 @@
 import random
-from utils.pelaaja import olio_luonti, Pelaaja
+from data.sql_db_update import sql_db_lookup_locations
+from utils.pelaaja import olio_luonti
 pelaaja = olio_luonti()
 
 
 def generate_delivery_location():
-    print(pelaaja.location)
+    from utils.pelaaja import olio_luonti
+    pelaaja = olio_luonti()
+    current_location = pelaaja.location
+    locations = sql_db_lookup_locations()
+    if locations:
+        delivery_location = random.choice(locations)[0]
+        if current_location == delivery_location:
+            generate_delivery_location()
+        return delivery_location
+    else:
+        return None
 
-generate_delivery_location()
+
+locaation=generate_delivery_location()
+print(locaation)

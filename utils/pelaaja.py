0d43b3d3d927a89cdc77b1f05c1957413536c77f
tonih <toni.hirvikallio@gmail.com>
2024-02-12 07:48:55 +0200

    Pelaaja.py korjattu. Toimii kaikissa tiedostoiss

--- utils/pelaaja.py
+++ utils/pelaaja.py
@@ -10,30 +10,31 @@ print(pelaaja.pisteet) = print(self."")
 
 # pelaajan tiedot
 class Pelaaja:
-    def __init__(self, nimi, pisteet, location):
-        self.nimi = nimi
-        self.pisteet = pisteet
-        self.location = location
-        self.tehtava_aktiivinen = False
+    _instance = None
 
-    # Päivittää pelaajan tehtävä tilan
+    def __new__(cls, nimi, pisteet, location):
+        if cls._instance is None:
+            cls._instance = super(Pelaaja, cls).__new__(cls)
+            cls._instance.nimi = nimi
+            cls._instance.pisteet = pisteet
+            cls._instance.location = location
+            cls._instance.tehtava_aktiivinen = False
+        return cls._instance
+
     def paivita_tehtava_aktiivinen(self, is_active):
         self.tehtava_aktiivinen = is_active
 
-    def paivita_pisteet(self, pisteet, piste_maara, kerroin):
-        self.pisteet = pisteet + piste_maara * kerroin
-
+    def paivita_pisteet(self, piste_maara, kerroin):
+        self.pisteet += piste_maara * kerroin
 
 def kayttaja_haku():
     tulos = sql_search(f"SELECT screen_name, co2_budget, location "
                        f"FROM game "
-                       f"where screen_name = 'ilkka'")
+                       f"WHERE screen_name = 'ilkka'")
     return tulos
 
-
-# luo olion, johon tallennetaan pelaajan tiedot
 def olio_luonti():
     res = kayttaja_haku()
     for i in res:
-        asd = Pelaaja(i[0], i[1], i[2])
-        return asd
+        pelaaja = Pelaaja(i[0], i[1], i[2])
+        return pelaaja

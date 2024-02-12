b6541d227cc0f2eac83e8a9f53006ed917e1c29e
Vernere <verner.etola@metropolia.fi>
2024-02-12 11:44:36 +0200

    sql_db_update.py kommenteja
    tehtavan_luonti_algoritmi.py

--- data/tehtavan_luonti_algoritmi.py
+++ data/tehtavan_luonti_algoritmi.py
@@ -18,14 +18,16 @@ def generate_delivery_location():
         return None
 
 
-location=generate_delivery_location()
+location = generate_delivery_location()
 print(location)
 
 def kerroin_maaarittaj(location):
     airport_location_info = sql_db_lookup_airport_info(location)
-    a_continent = airport_location_info[0]
-    a_type = airport_location_info[0]
-    a_country = airport_location_info[0]
-    print(a_continent, a_type, a_country)
+    a_continent = airport_location_info[0][0]
+    a_type = airport_location_info[0][1]
+    a_country = airport_location_info[0][2]
 
+    print(a_continent, a_type, a_country)
+    #if airport.type closed = generate_delivery_location()
+    #values =
 kerroin_maaarittaj(location)

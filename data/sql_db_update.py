2b796c003f598add674274dce89ed202433e538e
tonih <toni.hirvikallio@gmail.com>
2024-02-13 11:49:23 +0200

    pelaaja.py sql siirretty sql_db_update.py kansioon. pelaajan tietojen haku lisätty sql_db_update.py tiedostoon

--- data/sql_db_update.py
+++ data/sql_db_update.py
@@ -18,6 +18,11 @@ def sql_db_lookup_log_in(screen_name):#(player_password) ei ole vielä
     locations_search = f"SELECT g.id FROM game g WHERE g.screen_name = '{screen_name}';" # AND g.password = '{password_player}'
     game_id = sql_search(locations_search)
     return game_id
+def sql_db_lookup_kayttaja_tiedot(screen_name, salasana):
+    tulos = sql_search(f"SELECT screen_name, co2_consumed, location, co2_budget "
+                       f"FROM game "
+                       f"WHERE screen_name = 'ilkka'")
+    return tulos
 def sql_db_update_new_game(screen_name, player_password): #HUOM!!!!älä kutsu tätä, ei valmis
     update_query =  (f"INSERT INTO game (co2_consumed, co2_budget, screen_name, location, password)" #<--Password ei vielä lisätty DB
                      f"VALUES (0, 10000, '{screen_name}','EFHK', '{player_password}');")             #<---- Default starting location EFHK(Helsinki Vantaa)

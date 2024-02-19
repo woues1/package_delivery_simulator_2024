428702ccc946d357b96c489db694d6f22cba3838
Vernere <verner.etola@metropolia.fi>
2024-02-19 23:49:55 +0200

    peli.py lisätty while loop, ja valinnat. kaikki toimii pitsi pelaaja.suorita_tehtava() en tiiä haalusitko tehä eri taval tän main ku while loopis, mutt tää oli loogisin mulle
    valikko.py lisätty uusi_peli toiminto, mutta ei pysty vielä vaihtaa pelejä valikkosta. Onnko ideoita ?

--- peli.py
+++ peli.py
@@ -5,17 +5,20 @@ from utils.valikko import valikko
 
 
 def main():
+    jatka = True#Lisätty while loop että valikko toiminnallisuus toimii, en tiiä ootko samaaa mieltä tästä
     pelaaja = main_menu()
-    print(pelaaja.nimi)
+    while jatka:
+
+        print(pelaaja.nimi)
 
     # toimiva tehtävän luonti
 
-    t1 = luo_tehtava(pelaaja)
-    t2 = luo_tehtava(pelaaja)
-    t3 = luo_tehtava(pelaaja)
-    lista = [t1,t2,t3]
-    print(*lista[0])
-    print(f"""
+        t1 = luo_tehtava(pelaaja)
+        t2 = luo_tehtava(pelaaja)
+        t3 = luo_tehtava(pelaaja)
+        lista = [t1,t2,t3]#näihin pitäs vaihtaa airport.name mielummin kun airport.id
+        print(*lista[0])
+        print(f"""
         +------------------------------------------+
         |Tehtävät(Airport, Co2 Consumed, kerroin)  |
         +------------------------------------------+
@@ -23,7 +26,7 @@ def main():
         2.{t2} 
         3.{t3} 
         +---------------------+ 
-        |     Omat tiedot     |  
+        |     Omat tiedot     |  Aktiivinen tehtava : {pelaaja.current_tehtava}
         +---------------------+ 
                Country                                              
                 {pelaaja.location}                                  
@@ -31,9 +34,25 @@ def main():
                 {pelaaja.pisteet}         
         +---------------------+ 
         """)
-    valintaa = input("Valitse : 1.Vallikko, 2.Valitse tehtävä , 3.Siirry  ")#4.Kauppa? Vois käyttää raha saada permanent buffs, esim. Co2 consumed halved, pelaaja.pisteet doubler, Co2 Budget doubler.
-    if valintaa == "1":
-        valikko(pelaaja)
+        valinta = input("Valitse : 1.Valikko, 2.Valitse tehtävä , 3.Siirry  ")#4.Kauppa? Vois käyttää raha saada permanent buffs, esim. Co2 consumed halved, pelaaja.pisteet doubler, Co2 Budget doubler.
+        if valinta == "1":
+            valikko(pelaaja)
+            continue
+        if valinta == "2":
+            tehtava = input("Valitse tehtävä (1, 2, 3): ")
+            if tehtava == "1":
+                pelaaja.aseta_tehtava(t1)
+            elif tehtava == "2":
+                pelaaja.aseta_tehtava(t2)
+            elif tehtava == "3":
+                pelaaja.aseta_tehtava(t3)
+            else:
+                print(f"{tehtava} ei ole valinta.")
+        if valinta == "3":
+            pelaaja.suorita_tehtava()
+        else:
+            print(f"{valinta} ei ole valinta")
+
     #tehtava = Tehtava(t1[0],t1[1],t1[2])
     #pelaaja.aseta_tehtava(tehtava)
     #pelaaja.suorita_tehtava()
--- utils/valikko.py
+++ utils/valikko.py
@@ -9,10 +9,10 @@ def valikko():
     print("LOPETA PELI(nappi)")
 
 """
-from utils.pelaaja import olio_luonti
+import sys
+from data.sql_db_update import sql_db_update_new_game
 def valikko(pelaaja):
-    import sys
-    print(pelaaja.pisteet,pelaaja.nimi,pelaaja.location)
+
     while True:
         valinta = input("""
         +--------------------+
@@ -26,11 +26,15 @@ def valikko(pelaaja):
         if valinta == "1":
             #update.db
             print("uusi peli")#<----No clue mitetn tää pitäs tehä
+            screen_name = input("Käyttäjänimi: ")
+            player_password = input("Salasana : ")
+            pelaaja = sql_db_update_new_game(screen_name, player_password)
+            return pelaaja
         elif valinta== "2":
             print("Lopetetaan peli...")
-            #update.db
+            #update.db, tarviiks täs olla mitään?
             sys.exit()
         elif valinta == "3":
             print("Main")
-            return valikko(pelaaja)
+            return pelaaja
 

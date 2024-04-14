0ac55d6facd239a48e27619ea6f53fe058bd5f3f
Vernere <verner.etola@gmail.com>
2024-04-14 18:44:02 +0300

    Kommentit

--- JS/displaytextures.js
+++ JS/displaytextures.js
@@ -2,13 +2,23 @@ let textureData;
 const library ={
     background: "main_menu_background_corr.png",
 }
+
+// Fetch noutaa json datan, functio struktuuri ja .then käyttö on sen takia koska json data lataa hitaamin kun itse niiden piirtäminen näytölle
+
 fetch('../Assets/textures.json')
   .then(response => response.json())
   .then(data => {
-    textureData = data;
-    displayTexture('background', 'main_menu_background_corr.png');
-    displayTexture('header', 'header_corr.png');
-  });
+  textureData = data;
+
+  // Tässä yhdistyy html id ja kuva json tiedostosta esim. <div id="header" class="texture"></div>
+  // Nämä funktio kutsut on täällä juuri json datan hitaan lataamisen takia
+
+  displayTexture('background', 'main_menu_background_corr.png');
+  displayTexture('header', 'header_corr.png');
+});
+
+
+// Funktio joka piirtää näytölle kuvan, ja lisää niihin koon ja position.
 
 function displayTexture(elementId, textureName){
     const texture = textureData.frames.find(texture => texture.filename === textureName);
@@ -19,3 +29,4 @@ function displayTexture(elementId, textureName){
 }
 
 // library["background"]
+// Tällä hetkellä skripti on aika ei käyttäjäystävällinen, voidaan muokata käyttämällä arrayta.
